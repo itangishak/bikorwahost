c003a60adf47fb5f5056b80bc4f5541b991f4f1b
Itangishaka J.Esterique <48064130+itangishak@users.noreply.github.com>
2025-06-26 13:30:12 +0200

    Fix path to Settings in header

--- Bikorwa/src/views/layouts/header.php
+++ Bikorwa/src/views/layouts/header.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../utils/Settings.php';
+require_once __DIR__ . '/../../utils/Settings.php';
 if (!isset($pdo)) {
     $database = new Database();
     $pdo = $database->getConnection();
